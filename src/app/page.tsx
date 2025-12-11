3828ce5f69fa40d0eaa852d7867a6cbf4301cb1c
jeffRJ-RJ <jeff_ors@yahoo.com.br>
2025-12-11 17:30:42 -0300

    ajuste fundo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-   <>
+    <main className="min-h-screen bg-grid-pattern">
       <Header />
-   </>
+   </main>
   );
 }
